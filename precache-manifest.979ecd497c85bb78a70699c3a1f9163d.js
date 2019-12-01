1a14efe37a6820d15b2d60c39400acf6e41015b1
MannyIOI <aman.teferi.80@gmail.com>
2019-12-01 12:39:25 +0300

    Updates

---
+++ precache-manifest.979ecd497c85bb78a70699c3a1f9163d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a434efc23a7f0d84e8f5e642db76d1e0",
+    "url": "/MyGithubPage/index.html"
+  },
+  {
+    "revision": "6382f2b158cae0fd565e",
+    "url": "/MyGithubPage/static/css/main.13091011.chunk.css"
+  },
+  {
+    "revision": "f72133a8fa5ce51e9ffa",
+    "url": "/MyGithubPage/static/js/2.95710be8.chunk.js"
+  },
+  {
+    "revision": "6382f2b158cae0fd565e",
+    "url": "/MyGithubPage/static/js/main.2f0cdb2c.chunk.js"
+  },
+  {
+    "revision": "ed1eab802d0f1052b51d",
+    "url": "/MyGithubPage/static/js/runtime-main.b02399be.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MyGithubPage/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f357de7ad9f47e18afd8e8ede3da146c.js"
+  "/MyGithubPage/precache-manifest.979ecd497c85bb78a70699c3a1f9163d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyGithubPage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
